2ad4430f762ab99500c52ca9f2a96c612ec4c914
rishinandan08 <65062233+rishinandan08@users.noreply.github.com>
2021-06-01 19:34:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ const steps=[
       },
       {
         id: 'notes',
-        component: <a href="http://localhost/bot/notes/homepage.php" target="_blank" rel="noreferrer" class="w3-button w3-blue w3-hover-blue">Go to the page<br/>(Click here)</a>,
+        component: <a href="https://humanizerbot.herokuapp.com/notes/homepage.php" target="_blank" rel="noreferrer" class="w3-button w3-blue w3-hover-blue">Go to the page<br/>(Click here)</a>,
         asMessage: true,
         trigger: 'continue',
       },
@@ -109,13 +109,13 @@ const steps=[
       },
       {
         id: 'stt',
-        component: <a href="http://localhost/bot/converter.php" target="_blank" rel="noreferrer" class="w3-button w3-blue w3-hover-blue">Go to the page<br/>(Click here)</a>,
+        component: <a href="https://humanizerbot.herokuapp.com/converter.php" target="_blank" rel="noreferrer" class="w3-button w3-blue w3-hover-blue">Go to the page<br/>(Click here)</a>,
         asMessage: true,
         trigger: 'continue',
       },
       {
         id: 'tts',
-        component: <a href="http://localhost/bot/texttospeech.html" target="_blank" rel="noreferrer" class="w3-button w3-blue w3-hover-blue">Go to the page<br/>(Click here)</a>,
+        component: <a href="https://humanizerbot.herokuapp.com/texttospeech.html" target="_blank" rel="noreferrer" class="w3-button w3-blue w3-hover-blue">Go to the page<br/>(Click here)</a>,
         asMessage: true,
         trigger: 'continue',
       },
